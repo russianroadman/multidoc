5c62e2c8bf6afc8b109c695c980566ee5d93abf0
russianroadman <realgar2@gmail.com>
2021-06-30 21:38:41 +0800

    cosmetic changes

---
+++ src/main/java/tanto/multidoc/FaviconConfiguration.java
@@ -0,0 +1,34 @@
+package tanto.multidoc;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.web.servlet.handler.SimpleUrlHandlerMapping;
+import org.springframework.web.servlet.resource.ResourceHttpRequestHandler;
+
+import java.util.Collections;
+
+/**
+ * Just a class to see favicon in browser
+ * I have no idea how this works
+ * */
+@Configuration
+public class FaviconConfiguration {
+
+    @Bean
+    public SimpleUrlHandlerMapping customFaviconHandlerMapping() {
+        SimpleUrlHandlerMapping mapping = new SimpleUrlHandlerMapping();
+        mapping.setOrder(Integer.MIN_VALUE);
+        mapping.setUrlMap(Collections.singletonMap(
+                "/static/fav.png", faviconRequestHandler()));
+        return mapping;
+    }
+
+    @Bean
+    protected ResourceHttpRequestHandler faviconRequestHandler() {
+        ResourceHttpRequestHandler requestHandler
+                = new ResourceHttpRequestHandler();
+        requestHandler.setLocations(Collections.singletonList(new ClassPathResource("/")));
+        return requestHandler;
+    }
+}
